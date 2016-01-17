6ad983e2abcd3bc13b852e055c949f2d093c791e
jinux <jinguol999@163.com>
2016-01-17 22:07:25 +0800

    脚本可以拉去书籍信息，然后保存到json，就差保存成yaml了

--- script/node/app.js
+++ script/node/app.js
@@ -25,19 +25,58 @@ function main() {
     rl.close();
   });
 
-  /*
-	var bookfile = '../../_posts/读书/2016-01-6-book_List.md';
-	var json = readYaml(bookfile);
-	*/
 }
 
-function readYaml(path) {
+function readYaml(path, f) {
 	fs.readFile(path, function(err, fileContents) {
-	  fileContents = fileContents.toString()
-	  console.log('\n')
-	  console.log(fileContents);
-	  console.log('\noutputs:\n')
-	  console.log(yaml.eval(fileContents))
+		fileContents = fileContents.toString()
+		// 1. 分行
+		var lines = fileContents.split('\n');
+		var root = {};
+		var tree = root;
+		var stack = [];
+		stack.push(tree);
+		for(var i = 0; i< lines.length; i++) {
+			// 2. 分词， 一行的数据结构
+			var s = / */.exec(lines[i]);
+			var indent = s[0].length;
+			var has = lines[i].indexOf('-') > -1;
+			var content = lines[i].substring(indent + (has? 1:0));
+			var kv = function(a) {
+				var i = content.indexOf(':');
+				return [content.substring(0,i), content.substring(i+2)];
+			}(content);
+			var key = kv[0].trim();
+			var value = kv[1];
+			var lineStruct = [indent, has, key, value];
+			var lastArray;
+			// 3. 逐行转化为json数据
+			if (content == '--' || content == '') {
+			} else {
+				if (has) {
+					var tr = {};
+					lastArray.push(tr);
+					tree = tr;
+				}
+				if(value.trim() !== '') {
+					tree[key] = value;
+				} else {
+					if (i + 1 < lines.length) {
+						if(lines[i+1].indexOf('-') > -1) {
+							tree[key] = [];
+							lastArray = tree[key];
+							stack.push(tree);
+						} else {
+							tree[key] = value;
+						}
+					} else {
+						tree[key] = value;
+					}
+				}
+
+			}
+		}
+		f(root);
 	})
 }
 
@@ -51,10 +90,25 @@ function getBookInfo(url) {
 		var hints = getHints(infoNode.find('span.pl'), $);
 		var infoMap = mapInfo(infoText, hints);
 		var title = $('div#wrapper h1 span').text();
+		var picUrl;
 		infoNode.find('a.nbg').each(function($this){
-			var picUrl = $(this).attr('href');
+			picUrl = $(this).attr('href');
 			console.log("Get book face:" + picUrl);
-			request(picUrl).pipe(fs.createWriteStream(picDir+ title +'.jpg'));
+		});
+		
+		var info = {};	
+		info['title'] = title;
+		info['status'] = '在读';
+		info['author'] = infoMap['作者'];
+		info['publisher'] = infoMap['出版社:'];
+		info['language'] = '';
+		info['link'] = url;
+		info['cover'] = picUrl;
+
+		var bookfile = '../../_posts/读书/2016-01-6-book_List.md';
+		readYaml(bookfile, function(tree){
+			tree['books'].push(info);
+			console.log(tree);
 		});
 		
 	});
@@ -77,7 +131,6 @@ function mapInfo(text, keys) {
 			val = text.substring(poss[i]+keys[i].length, poss[i+1]);	
 		}
 		infoMap[keys[i]] =  val;
-		console.log(keys[i] +"--"+ val);
 	}
 	return infoMap;
 }
