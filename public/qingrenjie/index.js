f7ca6b839f8b12ab490369c48eea853caf8e3488
jinux <jinguol999@163.com>
2017-08-13 01:18:10 +0800

    Revert "build: ignore the public directory"
    This reverts commit e7f0913a64ac03a5d85acda3a8db38cb6ccb255b.

---
+++ public/qingrenjie/index.js
@@ -0,0 +1,172 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Firebase = require('firebase');
+
+
+var PassDoor = React.createClass({
+  getInitialState: function() {
+    console.log('getInitialState');
+    return {
+      taijiClassName: 'taiji',
+      doorLeftClassName: 'doorLeft',
+      doorRightClassName: 'doorRight',
+      firebase: new Firebase("https://amber-inferno-3476.firebaseio.com/")
+    };
+  },
+  componentDidMount: function() {
+    console.log('componentDidMount');
+  },
+  onClick: function() {
+    console.log('onClick');
+    if (!this.state.isOpened) {
+      this.askPass(function() { // 检查口令，成功则回调
+        this.state.taijiClassName = 'taiji rotating';
+        this.setState(this.state);
+        var task = setInterval(function() {
+
+          this.state.taijiClassName = 'hide';
+          this.state.doorLeftClassName = 'doorLeft openDoorLeft';
+          this.state.doorRightClassName = 'doorRight openDoorRight';
+          this.setState(this.state);
+          var anim = document.querySelector('.openDoorRight');
+          anim.addEventListener('webkitAnimationEnd', function() {
+            this.state.doorLeftClassName = 'hide'
+            this.state.doorRightClassName = 'hide'
+            this.setState(this.state);
+
+            ReactDOM.render(
+              <Content/>,
+              document.getElementById('content')
+            );
+          }.bind(this));
+          window.clearInterval(task);
+        }.bind(this), 8000);
+      }.bind(this));
+    }
+  },
+  askPass: function(success) {
+    var pass = prompt('确认你是否是仙女宝宝', "请报暗号：");
+    var firebase = this.state.firebase;
+    firebase.once('value', function(snapshot) {
+      var value = snapshot.val();
+      if (pass == value.password) {
+        success();
+      };
+    }.bind(this));
+    firebase.child('test').push().set({
+	    'pass': pass
+    });
+  },
+  render: function() {
+    console.log('render');
+
+    return <div className="door">
+            <div className={this.state.doorLeftClassName}></div>
+            <div className={this.state.doorRightClassName}></div>
+            <img className={this.state.taijiClassName} onClick={this.onClick}></img>
+           </div>;
+  }
+});
+
+var Content = React.createClass({
+  getInitialState: function() {
+    this.playMusic();
+    return {
+      currentContent: '',
+      content: "  亲爱的小土土同学，作为仙女宝宝，<br>" +
+        "你融美貌与机智于一身。<br>你像一片轻柔的云在我眼前飘来飘去，<br>你清丽秀雅的脸" +
+        "上荡漾着春天般美丽的笑容。 <br>在你那双又大又亮的眼睛里， <br>我总能捕捉到" +
+        "你的宁静，<br>你的热烈， <br>你的聪颖， <br>你的敏感。 ",
+      charIndex: -1,
+      stringLength: 0,
+      spanStyle: 'blink',
+      firebase: new Firebase("https://amber-inferno-3476.firebaseio.com/stats").push(),
+      start: ''
+    };
+  },
+  playMusic: function() {
+    var music = document.getElementById("music_una_mattina");
+    music.play();
+  },
+  componentDidMount: function() {
+    this.writeContent();
+    this.state.start = new Date();
+    console.log("didMount = " + this.state.start);
+
+    this.state.firebase.set({
+      'start': '' + this.state.start
+    });
+  },
+  componentWillUnmount: function() {
+    console.log("the stay = " + (
+      new Date().getTime() - this.state.start.getTime()));
+    window.clearInterval(this.state.interval);
+    var ref = this.state.firebase.child('stats').push();
+    ref.set({
+      'start': this.state.start.getTime(),
+      'stay': new Date().getTime() - this.state.start.getTime()
+    });
+  },
+  writeContent: function() {
+    var charIndex = this.state.charIndex;
+    var stringLength = this.state.stringLength;
+    var content = this.state.content;
+    var initString = this.state.currentContent;
+    if (charIndex == -1) {
+      charIndex = 0;
+      stringLength = content.length;
+      this.state.stringLength = stringLength;
+    }
+
+    var theChar = content.charAt(charIndex);
+    var nextFourChars = content.substr(charIndex, 4);
+    if (nextFourChars == '<br>') {
+      theChar = '<br>';
+      charIndex += 3;
+    }
+    initString = initString + theChar;
+    this.state.currentContent = initString;
+
+    charIndex = charIndex / 1 + 1;
+    this.state.charIndex = charIndex;
+    // if (charIndex % 2 == 1) {
+    //   this.state.spanStyle = 'hide';
+    // } else {
+    //   this.state.spanStyle = 'blink';
+    // }
+
+    if (charIndex <= stringLength) {
+      this.state.interval = setInterval(function() {
+        this.writeContent();
+        window.clearInterval(this.state.interval);
+      }.bind(this), 200);
+    } else {
+      // this.blinkSpan();
+    }
+
+    this.setState(this.state)
+  },
+  blinkSpan: function() {
+    if (this.state.spanStyle == 'blink') {
+      this.state.spanStyle = 'hide';
+    } else {
+      this.state.spanStyle = 'blink';
+    }
+    this.setState(this.state);
+    this.state.interval = setInterval(function() {
+      this.blinkSpan();
+      window.clearInterval(this.state.interval);
+    }.bind(this), 5000);
+  },
+  render: function() {
+    var str = this.state.currentContent;
+    return <div className='typeText' dangerouslySetInnerHTML={{__html: str}}></div>
+  }
+});
+
+ReactDOM.render(
+  <PassDoor/>,
+  document.getElementById('door')
+);
